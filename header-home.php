9af52998d15d0d2da548f92cf8701e97ffd65479
root <root@linux-ajvn.site>
2014-08-31 10:41:56 +0100

    Rewrote the gateway to have small display and medium display (Fixes #3) Now easier to add links (Use $(selector).makeFloat(), Fixes #3)

--- header-home.php
+++ header-home.php
@@ -1,40 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo wp_title(); ?></title>
+	<title>BeXcellent | Gateway</title>
 	<meta charset="windows-1252">
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	<style type="text/css">
+	html
+	{
+		overflow: hidden;
+	}
+	body
+	{
+		width: 100vw;
+		height: 100vh;
+	}
+	</style>
 </head>
 <body>
-	<div id ="l1" class="linkbubble " style="left: 145px; top: 155px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/videos"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/video.png" alt="Videos"></a>
-	</div> 
-	<div id ="l2" class="linkbubble " style="left: 295px; top: 200px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/articles.png" alt="Articles"></a>
+	<div class="visible-sm visible-xs">
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/videos"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/video.png" alt="Videos"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/articles.png" alt="Articles"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/about.png" alt="About"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/photos"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/photos.png" alt="Photos"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/games.png" alt="Games"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/news.png" alt="News"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/parents.png" alt="Parents"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/5-8"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/5-8.png" alt="5-8" style="width: 114px; height: 115px;"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/8-12"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/8-12.png" alt="8-12"></a>
+		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/12-18"><img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/12-18.png" alt="12-18"></a>
+
 	</div>
-	<div id ="l3" class="linkbubble " style="left: 400px; top: 280px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/about.png" alt="About"></a>
+	<div class="hidden-sm hidden-xs">
+		<div id ="l1" class="linkbubble" style="left: 145px; top: 155px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/videos"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/video.png" alt="Videos"></a>
+		</div> 
+		<div id ="l2" class="linkbubble" style="left: 295px; top: 200px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/articles.png" alt="Articles"></a>
+		</div>
+		<div id ="l3" class="linkbubble" style="left: 400px; top: 280px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/about.png" alt="About"></a>
+		</div>
+		<div id ="l4" class="linkbubble" style="left: 475px; top: 155px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/photos"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/photos.png" alt="Photos"></a>
+		</div>
+		<div id ="l5" class="linkbubble" style="left: 270px; top: 415px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/games.png" alt="Games"></a>
+		</div>
+		<div id ="l6" class="linkbubble" style="left: 555px; top: 365px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/news.png" alt="News"></a>
+		</div>
+		<div id ="l7" class="linkbubble" style="left: 695px; top: 470px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/parents.png" alt="Parents"></a>
+		</div>
+		<div id ="l8" class="linkbubble" style="left: 50px; top: 55px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/5-8"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/5-8.png" alt="5-8" style="width: 114px; height: 115px;"></a>
+		</div>
+		<div id ="l9" class="linkbubble" style="left: 585px; top: 40px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/8-12"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/8-12.png" alt="8-12"></a>
+		</div>
+		<div id ="l10" class="linkbubble" style="left: 95px; top: 480px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/12-18"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/12-18.png" alt="12-18"></a>
+		</div> 
+		<img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/becellent.jpg" alt="BeXcellent" class="" style="position:absolute; top: 120px; left: 2px;"/>
 	</div>
-	<div id ="l4" class="linkbubble " style="left: 475px; top: 155px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/photos"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/photos.png" alt="Photos"></a>
-	</div>
-	<div id ="l5" class="linkbubble " style="left: 270px; top: 415px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/games.png" alt="Games"></a>
-	</div>
-	<div id ="l6" class="linkbubble " style="left: 555px; top: 365px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/news.png" alt="News"></a>
-	</div>
-	<div id ="l7" class="linkbubble " style="left: 695px; top: 470px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/parents.png" alt="Parents"></a>
-	</div>
-	<div id ="l8" class="linkbubble " style="left: 50px; top: 55px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/5-8"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/5-8.png" alt="5-8" style="width: 114px; height: 115px;"></a>
-	</div>
-	<div id ="l9" class="linkbubble " style="left: 585px; top: 40px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/8-12"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/8-12.png" alt="8-12"></a>
-	</div>
-	<div id ="l10" class="linkbubble " style="left: 95px; top: 480px;">
-		<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/12-18"><img class="linkbubble " src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/12-18.png" alt="12-18"></a>
-	</div> 
-	<img src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/becellent.jpg" alt="BeXcellent" class="" style="position:absolute; top: 120px; left: 2px;" onload="start();" />
