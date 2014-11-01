abf2c918a32ff5e297d8da1b7abbac4247a1fce5
pm990320 <patmenlove@gmail.com>
2014-11-01 19:30:00 +0000

    ...

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (is_page('gateway')) {
 			<?php if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post(); ?>
-                                                <div style="text-align: center;">
+                                                <div id="wrapper" style="text-align: center;">
                                                         <h1 class="text-center"><?php the_title(); ?></h1>
                                                         <h6 id="thedate" class="text-center"><?php if (!is_page()) { echo the_time('F jS, Y'); } ?></h6>
                                                         <p id="postcontent" class="text-center">
