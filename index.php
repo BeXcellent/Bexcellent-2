44de71902ce1cf3e26cd0b2723423096642220d1
pm990320 <patmenlove@gmail.com>
2014-11-01 12:04:49 +0000

    trying something with php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (is_page('gateway')) {
 					the_post(); ?>
 					<p id="postcontent">
 						<h1 class="text-center"><?php the_title(); ?></h1>
-                                                <h6 id="thedate" class="text-center"><?php the_time('F jS, Y'); ?></h6>
+                                                <h6 id="thedate" class="text-center"><?php if (is_post()) { echo the_time('F jS, Y'); } ?></h6>
 						<?php 
 						the_content(); 
 						comments_template();
