21b5da45e64fba6b7d5ba7d5cdf62b0b76a809ba
pm990320 <patmenlove@gmail.com>
2014-11-04 14:53:14 +0000

    starting work on comments

--- index.php
+++ index.php
@@ -9,21 +9,27 @@ if (is_page('gateway')) {
 			<?php if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post(); ?>
-                                                <div id="wrapper">
-                                                        <h1 class="text-center"><?php the_title(); ?></h1>
-                                                        <h6 id="thedate" class="text-center"><?php if (!is_page()) { echo the_time('F jS, Y'); } ?></h6>
+                    
+                                <div id="wrapper">
+                                    <h1 class="text-center"><?php the_title(); ?></h1>
+                                    <h6 id="thedate" class="text-center"><?php if (!is_page()) { echo the_time('F jS, Y'); } ?></h6>
                                                         
-                                                        <?php echo the_content();  ?>
-                                                        
-                                                        <p style="<?php if(stripos(comments_template(), "comments are closed") !== false) { echo "display:none"; } ?>">
-                                                        <?php comments_template(); } } ?>
-                                                        </p>
-                                                </div>
-				<div id="google_translate_element"></div><script type="text/javascript">
-				function googleTranslateElementInit() {
+                                    <?php echo the_content();  ?>
+                                                       
+                                </div>
+                    
+                                <div id="comments">
+                                    <?php echo comments_template() ?>
+                                </div>
+                                
+				<div id="google_translate_element"></div>
+                                
+                                <script type="text/javascript">
+                                    function googleTranslateElementInit() {
 					new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-				}
-			</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                                    }
+                                </script>
+                        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 		</div>
 	</div>
 	<?php
