7be5df09cb8b14b26d5fb1110d225937128b2244
pm990320 <patmenlove@gmail.com>
2014-11-04 15:16:27 +0000

    starting work on comments

--- index.php
+++ index.php
@@ -1,39 +1,35 @@
 <?php 
 if (is_page('gateway')) {
 	get_header('gateway');
-}else{
+} else {
 	get_header();
 	?>
-	<div class="row">
-		<div id="pagecontent" class="col-md-10 col-md-offset-1">
+<div class="row">
+    <div id="pagecontent" class="col-md-10 col-md-offset-1">
 			<?php if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post(); ?>
-                    
-                                <div id="wrapper">
-                                    <h1 class="text-center"><?php the_title(); ?></h1>
-                                    <h6 id="thedate" class="text-center"><?php if (!is_page()) { echo the_time('F jS, Y'); } ?></h6>
-                                                        
-                                    <?php echo the_content();  ?>
-                                    
-                                    <?php comments_template(); ?>
-                                </div>
-                                
-				<div id="google_translate_element"></div>
-                                
-                                <script type="text/javascript">
-                                    function googleTranslateElementInit() {
-					new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-                                    }
-                                </script>
-                        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-		</div>
-	</div>
+                                            
+        <div id="wrapper">
+            <h1 class="text-center"><?php the_title(); ?></h1>
+            <h6 id="thedate" class="text-center"><?php if (!is_page()){echo the_time('F jS, Y');} ?></h6>
+                <?php the_content();  ?>                      
+                <p>
+                    <?php comments_template(); ?>
+                </p>
+        </div>
+        <div id="google_translate_element"></div><script type="text/javascript">
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+            }
+        </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    </div>
+</div>
 	<?php
+        }               
 }
 if (is_page('gateway')) {
 	get_footer('gateway');
 }else{
 	get_footer();
 }
-?>
