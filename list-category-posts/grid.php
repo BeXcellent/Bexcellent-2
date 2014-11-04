a1b14e4b31894874eabe39b7dcb6b3b7c93bc86e
pm990320 <patmenlove@gmail.com>
2014-11-04 13:31:30 +0000

    ...

--- list-category-posts/grid.php
+++ list-category-posts/grid.php
@@ -76,10 +76,10 @@ $lcp_display_output .= '<div class="catliststyle">';
  */
 foreach ($this->catlist->get_categories_posts() as $single){
   //Start a List Item for each post:
-  $lcp_display_output .= '<div><a href="'.giveHREF($this->get_post_title($single)).'">';
+  $lcp_display_output .= '<a href="'.giveHREF($this->get_post_title($single)).'"> <div> <a href="'.giveHREF($this->get_post_title($single)).'">';
 
   //Show the title and link to the post:
-  $lcp_display_output .= '<p>'.chopHTMLtags($this->get_post_title($single)).'</p>';
+  $lcp_display_output .= chopHTMLtags($this->get_post_title($single));
   //Post Thumbnail
   $lcp_display_output .= $this->get_thumbnail($single);
 
