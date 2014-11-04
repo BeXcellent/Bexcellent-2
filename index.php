4e2c32573010eaf4e6dfa4c7702cdcd7e2fc876d
pm990320 <patmenlove@gmail.com>
2014-11-04 15:18:44 +0000

    ...

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ if (is_page('gateway')) {
 			<?php if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post(); ?>
+<<<<<<< HEAD
                                             
         <div id="wrapper">
             <h1 class="text-center"><?php the_title(); ?></h1>
@@ -25,6 +26,25 @@ if (is_page('gateway')) {
         </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     </div>
 </div>
+=======
+                                                <div id="wrapper">
+                                                        <h1 class="text-center"><?php the_title(); ?></h1>
+                                                        <h6 id="thedate" class="text-center"><?php if (!is_page()) { echo the_time('F jS, Y'); } ?></h6>
+                                                        
+                                                        <?php echo the_content();  ?>
+                                                        
+                                                        <p style="<?php if(stripos(comments_template(), "comments are closed") !== false) { echo "display:none"; } ?>">
+                                                        <?php comments_template(); } } ?>
+                                                        </p>
+                                                </div>
+				<div id="google_translate_element"></div><script type="text/javascript">
+				function googleTranslateElementInit() {
+					new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+				}
+			</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+		</div>
+	</div>
+>>>>>>> parent of 21b5da4... starting work on comments
 	<?php
         }               
 }
