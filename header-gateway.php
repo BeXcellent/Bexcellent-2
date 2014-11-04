6942944848f7a376a120134e5ed4712f496d6494
pm990320 <patmenlove@gmail.com>
2014-11-04 17:15:45 +0000

    ...

--- header-gateway.php
+++ header-gateway.php
@@ -49,52 +49,62 @@
 	
 	<div class="hidden-sm hidden-xs">
 		<div id="l1" class="linkbubble" style="left: 145px; top: 155px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/videos"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Videos">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/videos">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/single_bubble.png" alt="Videos">
                             <p class="bubbletext">Videos</p>
                         </a>
 		</div> 
 		<div id="l2" class="linkbubble" style="left: 295px; top: 200px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Articles">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Articles">
                             <p class="bubbletext">Articles</p>
                         </a>
 		</div>
 		<div id="l3" class="linkbubble" style="left: 400px; top: 280px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="About">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="About">
                             <p class="bubbletext">About</p>
                         </a>
 		</div>
 		<div id="l4" class="linkbubble" style="left: 475px; top: 155px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/photos"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Photos">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/photos">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Photos">
                             <p class="bubbletext">Photos</p>
                         </a>
 		</div>
 		<div id="l5" class="linkbubble" style="left: 270px; top: 415px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Games">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Games">
                             <p class="bubbletext">Games</p>
                         </a>
 		</div>
 		<div id="l6" class="linkbubble" style="left: 555px; top: 365px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="News">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="News">
                             <p class="bubbletext">News</p>
                         </a>
 		</div>
 		<div id="l7" class="linkbubble" style="left: 695px; top: 470px; display:none;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Parents">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="Parents">
                             <p class="bubbletext">Parents</p>
                         </a>
 		</div>
 		<div id="l8" class="linkbubble" style="left: 50px; top: 55px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/5-8"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="5-8" style="width: 114px; height: 115px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/5-8">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="5-8" style="width: 114px; height: 115px;">
                             <p class="bubbletext">5-8</p>
                         </a>
 		</div>
 		<div id="l9" class="linkbubble" style="left: 585px; top: 40px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/8-12"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="8-12">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/8-12">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="8-12">
                             <p class="bubbletext">8-12</p>
                         </a>
 		</div>
 		<div id="l10" class="linkbubble" style="left: 95px; top: 480px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/12-18"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="12-18">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/12-18">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/bubble-1.jpg" alt="12-18">
                             <p class="bubbletext">12-18</p>
                         </a>
 		</div>
