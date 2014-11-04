f8c6e87d97fda9a5b987d45abfa3b3b5278362da
pm990320 <patmenlove@gmail.com>
2014-11-04 17:29:15 +0000

    ...

--- header-gateway.php
+++ header-gateway.php
@@ -84,10 +84,10 @@
                             <p class="bubbletext">News</p>
                         </a>
 		</div>
-		<div id="l7" class="linkbubble" style="left: 695px; top: 470px; display:none;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents">
-                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/single_bubble.png" alt="Parents">
-                            <p class="bubbletext">Parents</p>
+		<div id="l7" class="linkbubble" style="left: 695px; top: 470px;">
+			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/applying">
+                            <img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/texture/new_bubbles/single_bubble.png" alt="Apply">
+                            <p class="bubbletext">Apply Now!</p>
                         </a>
 		</div>
 		<div id="l8" class="linkbubble" style="left: 50px; top: 55px;">
