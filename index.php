e65dbd9cdbaa2004da262ed73ba7aef687fc0a18
pm990320 <patmenlove@gmail.com>
2014-11-04 16:15:18 +0000

    What was going on in my head??

--- index.php
+++ index.php
@@ -25,23 +25,7 @@ if (is_page('gateway')) {
         </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     </div>
 </div>
-                                                <div id="wrapper">
-                                                        <h1 class="text-center"><?php the_title(); ?></h1>
-                                                        <h6 id="thedate" class="text-center"><?php if (!is_page()) { echo the_time('F jS, Y'); } ?></h6>
-                                                        
-                                                        <?php echo the_content();  ?>
-                                                        
-                                                        <p>
-                                                        <?php comments_template(); } } ?>
-                                                        </p>
-                                                </div>
-				<div id="google_translate_element"></div><script type="text/javascript">
-				function googleTranslateElementInit() {
-					new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-				}
-			</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-		</div>
-	</div>
+                                                
 	<?php
         }               
 if (is_page('gateway')) {
