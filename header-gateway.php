a59da0149050ca7c496dce3e570be82f618ce4c6
pm990320 <patmenlove@gmail.com>
2014-11-05 19:23:52 +0000

    ...

--- header-gateway.php
+++ header-gateway.php
@@ -33,39 +33,6 @@
 	</style>
 </head>
 <body>
-    <!--
-        <div class="visible-sm visible-xs">
-		<div id="l1-small" class="linkbubble-small" style="left:0px; top: 0px;">
-                    <a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/videos"><img class="linkbubble" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="Videos"><p class="bubbletext">Videos</p></a>
-		</div> 
-		<div id="l2-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="Articles"><p class="bubbletext">Articles</p></a>
-		</div>
-		<div id="l3-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="About"><p class="bubbletext">About</p></a>
-		</div>
-		<div id="l4-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery/photos"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="Photos"><p class="bubbletext"></p>Photos</a>
-		</div>
-		<div id="l5-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="Games"><p class="bubbletext">Games</p></a>
-		</div>
-		<div id="l6-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="News"><p class="bubbletext">News</p></a>
-		</div>
-		<div id="l7-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="Parents"><p class="bubbletext">Parents</p></a>
-		</div>
-		<div id="l8-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/5-8"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="5-8" style="width: 114px; height: 115px;"><p class="bubbletext">5-8</p></a>
-		</div>
-		<div id="l9-small" class="linkbubble-small" style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/8-12"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="8-12"><p class="bubbletext"></p>8-12</a>
-		</div>
-		<div id="l10-small" class="linkbubble-small"  style="left:0px; top: 0px;">
-			<a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/12-18"><img class="linkbubble-small" src="<?php echo get_template_directory_uri(); ?>/wp-content/uploads/2014/03/bubble-1.jpg" alt="12-18"><p class="bubbletext"></p>12-18</a>
-		</div>
-	</div> -->
 	
 	<div class="">
 		<div id="l1" class="linkbubble" style="left: 145px; top: 155px;">
