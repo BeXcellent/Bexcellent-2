d3b96c3249eca92e7388a3e239640b6420067273
pm990320 <patmenlove@gmail.com>
2014-11-06 14:32:06 +0000

    added 404 page and removed unnecessary files

---
+++ 404.php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+        <img src="<?php echo get_template_directory_uri(); ?>/texture/404.jpg">
+        <h5>Sorry, the page you are looking for was not found :(</h5>
+    </div>
+</div>
