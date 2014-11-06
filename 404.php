14d451ec5800a5893a1b63b417ec5d1e3e372ecc
pm990320 <patmenlove@gmail.com>
2014-11-06 14:36:46 +0000

    404 improvements

--- 404.php
+++ 404.php
@@ -5,9 +5,9 @@ if (is_page('gateway')) {
 	get_header();
 	?>
 <div class="row">
-    <div id="pagecontent" class="col-md-10 col-md-offset-1">
-		<img src="<?php echo get_template_directory_uri(); ?>/texture/404.jpg">
-                <h5>Sorry, the page you are looking for was not found :(</h5>	
+    <div id="pagecontent" class="col-md-10 col-md-offset-1" style="text-align: center;">
+        <h2>Sorry, the page you are looking for was not found :(</h2>	
+        <img src="<?php echo get_template_directory_uri(); ?>/texture/404.jpg">
     </div>
 </div>
                                    
