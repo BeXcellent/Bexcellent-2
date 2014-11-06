ebe74202cd023de3bbe7c7ddfd7d8a53e6bf3022
pm990320 <patmenlove@gmail.com>
2014-11-06 09:59:18 +0000

    ...

--- comments.php
+++ comments.php
@@ -1,16 +1,5 @@
 <?php if (comments_open(get_the_id())) : ?>
 <div id="disqus_thread"></div>
-<script type="text/javascript">
-    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqus_shortname = 'bexcellent'; // Required - Replace example with your forum shortname
-
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+<script type="text/javascript" async="true" src="//bexcellent.disqus.com/embed.js"></script>
+<p> if you are seeing this - good! </p>
 <?php endif; // comments_open ?>
