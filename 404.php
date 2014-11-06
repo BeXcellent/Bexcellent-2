2e237eec05002b808fc6ba126fc1c26a089f543e
pm990320 <patmenlove@gmail.com>
2014-11-06 14:35:31 +0000

    404 improvements

--- 404.php
+++ 404.php
@@ -1,6 +1,21 @@
+<?php 
+if (is_page('gateway')) {
+	get_header('gateway');
+} else {
+	get_header();
+	?>
 <div class="row">
-    <div class="col-md-10 col-md-offset-1">
-        <img src="<?php echo get_template_directory_uri(); ?>/texture/404.jpg">
-        <h5>Sorry, the page you are looking for was not found :(</h5>
+    <div id="pagecontent" class="col-md-10 col-md-offset-1">
+		<img src="<?php echo get_template_directory_uri(); ?>/texture/404.jpg">
+                <h5>Sorry, the page you are looking for was not found :(</h5>	
     </div>
 </div>
+                                   
+	<?php
+}               
+if (is_page('gateway')) {
+	get_footer('gateway');
+}else{
+	get_footer();
+}
+?>
