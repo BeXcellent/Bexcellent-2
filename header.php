7a85ba27e30a84bbfcbaac77c77c156c75a16d6d
oscar.shrimpton.personal@gmail.com <oscar.shrimpton.personal@gmail.com>
2015-11-28 12:51:29 +0000

    ../..

--- header.php
+++ header.php
@@ -38,32 +38,34 @@
 		</div>
 		<div class="row" id="menurow">
 			<div class="col-sm-12">
-				<nav class="navbar navbar-default transparent">
-					<div class="container-fluid">
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div>
+				<div class="navbar navbar-default transparent">
+					<nav class="navbar-inner">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
 
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav">
-								
-								<?php
-								wp_nav_menu( array(
-									'theme_location'    => 'primary',
-									'container'			=> false,
-									'items_wrap'		=> '%3$s'
-									));
-									?>
-								</ul>
-							</div><!-- /.navbar-collapse -->
-						</div><!-- /.container-fluid -->
-					</nav>
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									
+									<?php
+									wp_nav_menu( array(
+										'theme_location'    => 'primary',
+										'container'			=> false,
+										'items_wrap'		=> '%3$s'
+										));
+										?>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+							</div><!-- /.container-fluid -->
+						</nav>
+					</div>
 				</div>
 			</div>
