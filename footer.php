a55dc9981b34af1f9a7ddc0d7c21cf44a9757011
oscar.shrimpton.personal@gmail.com <oscar.shrimpton.personal@gmail.com>
2015-12-05 12:07:29 +0000

    Added Background Image Preloading (#44)

--- footer.php
+++ footer.php
@@ -16,6 +16,12 @@ function changeBackground() {
 	if(bgcount === 12) bgcount = 1;
 }
 </script>
+<div id="preloaded-images">
+	<?php for ($i = 1; $i <= 11; $i++): ?>
+
+   <img src="<?php echo get_template_directory_uri(); ?>/texture/backgrounds/bg<?php echo $i; ?>.png" width="1" height="1" alt="" />
+	<?php endfor; ?>
+</div>
 <style type="text/css">
 body
 {
